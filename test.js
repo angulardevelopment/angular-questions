549111e8a839ff06e87b719b8bd66c934ec5bf95
sunny <sunnyvales789@gmail.com>
2021-08-05 10:28:02 +0530

    Update test.js

--- test.js
+++ test.js
@@ -14,3 +14,12 @@ const sub1 = new Subject();
             });
 
 console.log(output)
+
+
+You have defined a link with Id = "linkA" in your Angular application. You want the link to be clickable but should not open the specified path. 
+Which of the following options must be followed for doing the same?
+ 
+Use "disabled" keyword in the definition of the link.
+Use the stopPropagation() method in the event listener of the click event for the link
+Use the preventDefault() method in the event listener of the click event for the link.
+Use "empty" keyword in the definition of the link.
